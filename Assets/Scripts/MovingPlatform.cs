4ef9f0bb742d674f15a7ce0c66c1ade2057a9ed8
Zach North <zrnorth@gmail.com>
2020-12-08 01:21:08 -0500

    Updating MovingPlatform.cs to correctly move anyone riding on it.

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -7,7 +7,8 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField]
     private float _platformSpeed = 1f;
 
-    private Transform _platform;
+    private List<Transform> _riders; // Other items "riding" the platform. They should move with it.
+
     private Transform _pointA;
     private Transform _pointB;
     private Transform _target;
@@ -24,24 +25,49 @@ public class MovingPlatform : MonoBehaviour
         }
     }
 
-    void Start()
+    private void Start()
     {
-        _platform = transform.Find("Platform");
-        _pointA = transform.Find("Point A");
-        _pointB = transform.Find("Point B");
+        _pointA = transform.parent.Find("Point A");
+        _pointB = transform.parent.Find("Point B");
         _target = _pointA;
+        _riders = new List<Transform>();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         float step = _platformSpeed * Time.deltaTime;
 
         // If we have reached the target, we start moving towards the other target.
-        if (Vector2.Distance(_platform.transform.position, _target.position) < .001f)
+        if (Vector2.Distance(transform.position, _target.position) < .001f)
         {
             SwapTargets();
         }
+        Vector2 oldPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 newPos = Vector2.MoveTowards(transform.position, _target.position, step);
+        Vector2 mvmt = newPos - oldPos;
 
-        _platform.transform.position = Vector2.MoveTowards(_platform.transform.position, _target.position, step);
+        transform.position = newPos;
+
+        foreach (Transform rider in _riders)
+        {
+            if (!rider)// If the rider got destroyed somehow
+            {
+                _riders.Remove(rider);
+                break;
+            }
+
+            rider.transform.position = new Vector2(rider.transform.position.x + mvmt.x,
+                                                   rider.transform.position.y + mvmt.y);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _riders.Add(other.transform);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        _riders.Remove(other.transform);
     }
 }
