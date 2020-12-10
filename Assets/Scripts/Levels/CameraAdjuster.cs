79b98d0507b4b19af3beae1eb873830a68a3ea6c
Zach North <zrnorth@gmail.com>
2020-12-10 01:38:53 -0500

    Added camera adjustment point prefab

---
+++ Assets/Scripts/Levels/CameraAdjuster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdjuster : MonoBehaviour
+{
+    [SerializeField]
+    private CameraController _camera;
+    [SerializeField]
+    private bool _updateCameraSettings = false;
+    [SerializeField]
+    private CameraSettings _newCameraSettings;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (_updateCameraSettings)
+            {
+                _camera.SetCameraSettings(_newCameraSettings);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+
+}
--- Assets/Scripts/Levels/CameraController.cs
+++ Assets/Scripts/Levels/CameraController.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct CameraSettings
+{
+    // Const scalars
+    public float xOffset;
+    public float yOffset;
+    public float trackingSpeed;
+    public bool lockXPosition;
+    public bool lockYPosition;
+}
+
 public class CameraController : MonoBehaviour
 {
     // References
     [SerializeField]
     private Transform _trackingTarget;
 
-    // Const scalars
     [SerializeField]
-    private float _xOffset = 0f;
-    [SerializeField]
-    private float _yOffset = 0f;
-    [SerializeField]
-    private float _trackingSpeed = 1f;
-    [SerializeField]
-    private bool _lockXPosition = false;
-    [SerializeField]
-    private bool _lockYPosition = false;
+    private CameraSettings _cameraSettings;
 
     // Private vars
     private float _xOffsetOriginal;
@@ -26,24 +28,24 @@ public class CameraController : MonoBehaviour
 
     private void Start()
     {
-        _xOffsetOriginal = _xOffset;
-        _yOffsetOriginal = _yOffset;
+        _xOffsetOriginal = _cameraSettings.xOffset;
+        _yOffsetOriginal = _cameraSettings.yOffset;
     }
 
     void Update()
     {
-        float xTarget = _trackingTarget.position.x + _xOffset;
-        float yTarget = _trackingTarget.position.y + _yOffset;
+        float xTarget = _trackingTarget.position.x + _cameraSettings.xOffset;
+        float yTarget = _trackingTarget.position.y + _cameraSettings.yOffset;
         // Note that the z does not update. it is fixed in the editor.
         float xNew = transform.position.x;
         float yNew = transform.position.y;
-        if (!_lockXPosition)
+        if (!_cameraSettings.lockXPosition)
         {
-            xNew = Mathf.Lerp(transform.position.x, xTarget, Time.deltaTime * _trackingSpeed);
+            xNew = Mathf.Lerp(transform.position.x, xTarget, Time.deltaTime * _cameraSettings.trackingSpeed);
         }
-        if (!_lockYPosition)
+        if (!_cameraSettings.lockYPosition)
         {
-            yNew = Mathf.Lerp(transform.position.y, yTarget, Time.deltaTime * _trackingSpeed);
+            yNew = Mathf.Lerp(transform.position.y, yTarget, Time.deltaTime * _cameraSettings.trackingSpeed);
         }
 
         transform.position = new Vector3(xNew, yNew, transform.position.z);
@@ -55,11 +57,16 @@ public class CameraController : MonoBehaviour
         // to look ahead the appropriate direction.
         if (dir == Direction.EAST)
         {
-            _xOffset = _xOffsetOriginal;
+            _cameraSettings.xOffset = _xOffsetOriginal;
         }
         else if (dir == Direction.WEST)
         {
-            _xOffset = _xOffsetOriginal * -1;
+            _cameraSettings.xOffset = _xOffsetOriginal * -1;
         }
+    }
+
+    public void SetCameraSettings(CameraSettings cameraSettings)
+    {
+        _cameraSettings = cameraSettings;
     }
 }
