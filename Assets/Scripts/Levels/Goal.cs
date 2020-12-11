2a9240f3ab9287a9a819097eef1f9f0013ec7fc5
Zach North <zrnorth@gmail.com>
2020-12-11 03:29:49 -0500

    Added Goal prefab and script

---
+++ Assets/Scripts/Levels/Goal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotateSpeed = 1f;
+    [SerializeField]
+    private int _sceneToLoad = 0;
+
+    private Transform _outer, _inner;
+
+    void Start()
+    {
+        _outer = GameObject.Find("White Square").GetComponent<Transform>();
+        _inner = GameObject.Find("Black Square").GetComponent<Transform>();
+    }
+    void Update()
+    {
+        // The goal spins slightly to catch the eye
+        _outer.transform.Rotate(new Vector3(0, 0, _rotateSpeed * Time.deltaTime));
+        _inner.transform.Rotate(new Vector3(0, 0, -1 * _rotateSpeed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(_sceneToLoad);
+        }
+    }
+}
